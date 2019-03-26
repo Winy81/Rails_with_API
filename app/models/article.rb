8f8357241d25fcce3f5f2c066735f925ecfe5abe
iSmash <nemeth.richo@gmail.com>
2019-03-26 15:21:57 +0000

    validations for slug

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	validates_presence_of :title, message: 'can not be blank'
 	validates_presence_of :content, message: 'can not be blank'
-	
+	validates_presence_of :slug, message: 'can not be blank'
+	validates_uniqueness_of :slug, message: 'should be uniqe'
 end
