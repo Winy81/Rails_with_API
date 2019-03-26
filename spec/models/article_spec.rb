2544c17d2e2bc6cc27094a3f97d50aaa1311d118
iSmash <nemeth.richo@gmail.com>
2019-03-26 13:41:22 +0000

    test for Article model validation

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should be that factory is valid' do
+  	expect(FactoryBot.build :article).to be_valid
+  end
+
+  it 'should validate the presence of the tilte' do
+  	article = FactoryBot.build :article, title:""
+  	expect(article).not_to be_valid
+  	expect(article.errors.message[:title]).to include("can be blank")
+  end
+
+  it 'should validate the presence of the content' do
+  	article = FactoryBot.build :article, content:""
+  	expect(article).not_to be_valid
+  	expect(article.errors.message[:content]).to include("can be blank")
+  end
+
+
 end
