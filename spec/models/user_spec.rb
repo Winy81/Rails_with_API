2962b67eff5e1abfd5917ea22921d996fd432043
iSmash <nemeth.richo@gmail.com>
2019-03-30 16:02:43 +0000

    test refatoring for users and more test for them

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,14 +2,28 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "#validations" do
-  	it "shuold be presence of attributes" do
+
+  	it "shuold be valid" do
   		user = FactoryBot.create :user
   		expect(user).to be_valid
+  	end
 
+
+  	it "shuold be presence of attributes" do
   		user = FactoryBot.build :user, login: nil, provider: nil
   		expect(user).not_to be_valid
   		expect(user.errors.messages[:login]).to include("can not be blank")
   		expect(user.errors.messages[:provider]).to include("can not be blank")
   	end
+
+  	it 'should be uniqe' do
+  		user = FactoryBot.create :user
+  		user2 = FactoryBot.build :user, login: user.login
+  		expect(user2).not_to be_valid
+
+  		user2.login = 'new_login'
+  		expect(user2).to be_valid
+  	end
+  	
   end
 end
