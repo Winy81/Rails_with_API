f088092a0cdc77733364e4329d8e3a246c790929
iSmash <nemeth.richo@gmail.com>
2019-03-30 15:46:52 +0000

    User first validation test and factories user.rb refactor for uniqe login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    login { "MyString" }
-    name { "MyString" }
-    url { "MyString" }
-    avatar_url { "MyString" }
-    provider { "MyString" }
+    sequence(:login) { |n| "jdo #{n}" }
+    name { "John Do" }
+    url { "http://example.com" }
+    avatar_url { "http://example.com/avatar" }
+    provider { "GitHub" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#validations" do
+  	it "shuold be presence of attributes" do
+  		user = FactoryBot.create :user
+  		expect(user).to be_valid
+  	end
+  end
 end
