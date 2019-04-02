b8b497d7e6aea8c972d91471597d0cc7538f5d38
iSmash <nemeth.richo@gmail.com>
2019-04-02 09:25:03 +0100

    make the basic test for registered user passed

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -48,7 +48,7 @@ describe UserAuthenticator do
 	 		end
 
 	 		it 'should reuse already registered user' do
-	 			user = FactoryBot.create, user_data
+	 			user = FactoryBot.create :user, user_data
 	 			expect{ subject }.not_to change{User.count}
 	 			expect(authenticator.user).to eq(user)
 	 		end
