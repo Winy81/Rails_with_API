8b1bc7d6fcb39ee3dd417eeb2d018f803dabfb0a
iSmash <nemeth.richo@gmail.com>
2019-04-05 10:28:30 +0100

    Code refactoring

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,8 +1,8 @@
 class AccessTokensController < ApplicationController
+ 
+ def create
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
+ end
 
-	def create
-		render json: {}, status: 401
-	end
-	
-	
 end
