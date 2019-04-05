c25ea6c48fd20c672daef8a80a90f5d2ad0f8dae
iSmash <nemeth.richo@gmail.com>
2019-04-05 12:58:27 +0100

    test for correct status code return when the access_token requiest is successful

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -30,6 +30,8 @@ RSpec.describe AccessTokensController, type: :controller do
 
 
 
+
+
 	context 'when no code provided' do
       subject { post :create }
       it_behaves_like "unauthorized_requests"
@@ -50,7 +52,11 @@ RSpec.describe AccessTokensController, type: :controller do
     end
 
     context 'when success request' do
-
+      subject { post :create, params: { code: 'valid_code' } }
+    	it 'should return 201 status code' do
+        subject
+        expect(response).to have_http_status(:created)
+      end
     end
 end 
 end
