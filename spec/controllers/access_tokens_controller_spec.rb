0f4f24d51c20f4b865da5829563c8bccd89a033d
iSmash <nemeth.richo@gmail.com>
2019-04-05 09:56:22 +0100

    test for error

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -4,10 +4,27 @@ RSpec.describe AccessTokensController, type: :controller do
 
 	describe '#create' do
  	  context 'when invalid request' do
+
+ 	  	let(:error) do
+        {
+          "status" => "401",
+          "source" => { "pointer" => "/code" },
+          "title" =>  "Authentication code is invalid",
+          "detail" => "You must provide valid code in order to exchange it for token."
+        }
+        end
+
+
 		it 'should return 401 status code' do
       	  post :create
           expect(response).to have_http_status(401)
        	end
+
+       	it 'should return proper error body' do
+          post :create
+          json = JSON.parse(response.body)
+          expect(json['errors']).to include(error)
+        end
 
 
 	  end 
