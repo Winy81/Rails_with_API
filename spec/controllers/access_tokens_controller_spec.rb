02f98d7ee34705f79ae81be0e0f6aac4fadd7617
iSmash <nemeth.richo@gmail.com>
2019-04-05 13:47:00 +0100

    test for 'shuold return proper jason body' on access_tokens_controller_spec.rb

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -75,6 +75,16 @@ RSpec.describe AccessTokensController, type: :controller do
      	    subject
        	    expect(response).to have_http_status(:created)
         end
+
+         it 'should return proper json body' do
+       		 expect{ subject }.to change{ User.count }.by(1)
+       		 user = User.find_by(login: 'jdoe1')
+       		 json = JSON.parse(response.body)
+       		 json_data = json['data']
+       		 expect(json_data['attributes']).to eq(
+          { 'token' => user.access_token.token }
+        )
+        end
     end
 end 
 end
