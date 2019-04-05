450628dd9ae8283306fbe5043206ecd63cdb3f4b
iSmash <nemeth.richo@gmail.com>
2019-04-05 13:19:17 +0100

    make the test passed fast

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -3,6 +3,9 @@ class AccessTokensController < ApplicationController
  def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
+
+
+    render json: {}, status: :created
  end
 
 end
