05b30424640cf095eb7c850f7893d1ba9d665ac3
iSmash <nemeth.richo@gmail.com>
2019-04-07 12:08:21 +0100

    more test for access_token destroy action

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe AccessTokensController, type: :controller do
 
-	describe '#create' do
+	describe 'POST #create' do
  	  shared_examples_for "unauthorized_requests" do
-		let(:error) do
+		let(:authentication_error) do
         {
           "status" => "401",
           "source" => { "pointer" => "/code" },
@@ -22,7 +22,7 @@ RSpec.describe AccessTokensController, type: :controller do
        	it 'should return proper error body' do
           post :create
           json = JSON.parse(response.body)
-          expect(json['errors']).to include(error)
+          expect(json['errors']).to include(authentication_error)
         end
 
     end
@@ -81,12 +81,43 @@ RSpec.describe AccessTokensController, type: :controller do
        			 user = User.find_by(login: 'jdoe1')
        			 json = JSON.parse(response.body)
        			 json_data = json['data']
-       			 pp json_data
-       			 pp json
        			 expect(json_data['attributes']).to eq(
           			{ 'token' => user.access_token.token }
         )
         end
     end
 end 
+
+
+	describe 'DELETE #destroy' do
+
+	 context 'when invalid request' do
+      let(:authorization_error) do
+        {
+          "status" => "403",
+          "source" => { "pointer" => "/headers/authorization" },
+          "title" =>  "Not authorized",
+          "detail" => "You have no right to access this resource."
+        }
+      end
+
+      subject { delete :destroy }
+
+      it 'should return 403 status code' do
+        subject
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'should return proper error json' do
+        subject
+        expect(json['errors']).to include(authorization_error)
+      end
+    end
+
+    context 'when valid request' do
+
+    end
+  end
+
+
 end
