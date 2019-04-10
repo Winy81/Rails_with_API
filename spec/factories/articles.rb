5e32dea9b26008ffc7ded183f4911af9886e8f97
iSmash <nemeth.richo@gmail.com>
2019-04-10 14:38:08 +0100

    add the association in the articles factory the solve the test crashes

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     sequence(:title) { |n| "MyString #{n}" }
     sequence(:content) { |n| "MyText #{n}" }
     sequence(:slug) { |n| "MySlug #{n}" }
+    association :user
   end
 end
