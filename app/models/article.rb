6c27e7e93f63bc1ec7837673096c19b526be0a16
iSmash <nemeth.richo@gmail.com>
2019-04-10 14:31:26 +0100

    generate migration and add user column for article. Associacion setup between them

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,7 @@ class Article < ApplicationRecord
 	validates_presence_of :slug, message: 'can not be blank'
 	validates_uniqueness_of :slug, message: 'should be uniqe'
 
+	belongs_to :user
+
 	scope :recent, -> {order(created_at: :desc)}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
 	validates_presence_of :provider, message: 'can not be blank'
 	validates_uniqueness_of :login, message: "should be uniqe"
 
+	has_many :article, dependent: :destroy
+
 	has_one :access_token, dependent: :destroy
 
 end
