b6a32dec1f2237714e4d4e401fc19a2bee259043
iSmash <nemeth.richo@gmail.com>
2019-04-10 15:54:54 +0100

    make the test passed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-    article = Article.new(article_params)
+    article = current_user.article.build(article_params)
     	article.save!
     	  render json: article, status: :created
   			rescue
@@ -22,13 +22,15 @@ class ArticlesController < ApplicationController
  	end
 
  	def update
-    	article = Article.find(params[:id])
+    	article = current_user.article.find(params[:id])
    		article.update_attributes!(article_params)
     	render json: article, status: :ok
- 		  rescue
-    	render json: article, adapter: :json_api,
-          serializer: ErrorSerializer,
-          status: :unprocessable_entity
+    	rescue ActiveRecord::RecordNotFound
+          authorization_error
+ 		rescue
+    	  render json: article, adapter: :json_api,
+            serializer: ErrorSerializer,
+            status: :unprocessable_entity
     end
 
   private
