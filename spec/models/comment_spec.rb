ea6d168403175c982832a6817dfaabded4276f57
iSmash <nemeth.richo@gmail.com>
2019-04-13 13:48:02 +0100

    comment_spec.rb test correction, comments.rb factory refactoring

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   describe '#validations' do
   	it 'shuold have valid factory' do
-  		expect(build :comment).to be_valid
+  		expect(FactoryBot.build :comment).to be_valid
   	end
   end
 end
