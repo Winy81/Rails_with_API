f033706a230d1d38f58360f170b6ea08df532f64
iSmash <nemeth.richo@gmail.com>
2019-04-13 14:10:07 +0100

    comment validation test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,15 @@ RSpec.describe Comment, type: :model do
   	it 'shuold have valid factory' do
   		expect(FactoryBot.build :comment).to be_valid
   	end
+
+  	it 'should be presence of attributes' do
+  		comment = Comment.new
+  		expect(comment).not_to be_valid
+  		expect(comment.errors.messages).to include({
+  			user: ['must exist'],
+  			article: ['must exist'],
+  			content: ["can not be blank"]
+  		})
+  	end
   end
 end
