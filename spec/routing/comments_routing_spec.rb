6ed0bf7a003e8ffc3fb27b9e57780b0e416d03e3
iSmash <nemeth.richo@gmail.com>
2019-04-13 18:40:10 +0100

    test correction on the commnets_routing_spec.rb

--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe CommentsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "articles/1/comments").to route_to("comments#index")
+      expect(:get => "articles/1/comments").to route_to("comments#index", article_id:'1')
     end
 
 
     it "routes to #create" do
-      expect(:post => "articles/1/comments").to route_to("comments#create")
+      expect(:post => "articles/1/comments").to route_to("comments#create", article_id:'1')
     end
 
     
