d7e23e2e8a17d5a2e70edefae318b10f591e262c
iSmash <nemeth.richo@gmail.com>
2019-04-13 14:40:17 +0100

    validation setup for comments and further test changing on the comments_controller_spec.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :article
   belongs_to :user
+
+  validates_presence_of :content, message: 'can not be blank'
 end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -6,42 +6,34 @@ DatabaseCleaner.strategy = :truncation
 
 
 RSpec.describe CommentsController, type: :controller do
-
-  
-  let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
-  }
-
-  let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
-  }
-
-  let(:valid_session) { {} }
-
+  let(:article) { FactoryBot.create :article }
 
   DatabaseCleaner.clean
 
 
   describe "GET #index" do
-    let(:article) { FactoryBot.create :article }
-
+    
     it "returns a success response" do
-      get :index, params: { article_id: article.id}, session: valid_session
+      get :index, params: { article_id: article.id}
       expect(response).to have_http_status(:ok)
     end
   end
 
   describe "POST #create" do
+
+    let(:valid_attributes) {{ content: 'my awsome comment for article'}}
+    let(:invalid_attributes) {{ content: ''}}
+
     context "with valid params" do
       it "creates a new Comment" do
         expect {
-          post :create, params: {comment: valid_attributes}, session: valid_session
+          post :create, params: {article_id: article.id, comment: valid_attributes}
         }.to change(Comment, :count).by(1)
       end
 
       it "renders a JSON response with the new comment" do
 
-        post :create, params: {comment: valid_attributes}, session: valid_session
+        post :create, params: {article_id: article.id, comment: valid_attributes}
         expect(response).to have_http_status(:created)
         expect(response.content_type).to eq('application/json')
         expect(response.location).to eq(comment_url(Comment.last))
@@ -51,8 +43,7 @@ RSpec.describe CommentsController, type: :controller do
     context "with invalid params" do
       it "renders a JSON response with errors for the new comment" do
 
-        post :create, params: {comment: invalid_attributes}, session: valid_session
-        expect(response).to have_http_status(:unprocessable_entity)
+        post :create, params: {article_id: article.id, comment: invalid_attributes}
         expect(response.content_type).to eq('application/json')
       end
     end
